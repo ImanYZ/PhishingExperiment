cd7196a8f4655ee0200c14535be92e52d4714847
Iman <oneweb@umich.edu>
2016-01-05 18:33:05 -0500

    Created the static and media folders.

--- UMSecurity/scripts/createadmin.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from django.contrib.auth.models import User
-if User.objects.count() == 0:
-    admin = User.objects.create(username='Iman')
-    admin.set_password('i1111114')
-    admin.is_superuser = True
-    admin.is_staff = True
-    admin.save()
