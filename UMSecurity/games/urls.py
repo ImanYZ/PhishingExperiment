5a1a06524fc85d3debfc62155b91dc8c40904233
Iman <oneweb@umich.edu>
2016-02-19 02:39:37 -0500

    Catch all unrecognized URLs and rediret them to login page.

--- UMSecurity/games/urls.py
+++ UMSecurity/games/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     url(r'^returning/final$', views.final, name='final'),
     url(r'^thankyou/$', views.thankyou, name='thankyou'),
     url(r'^thankyou/submit$', views.thankyousubmit, name='thankyousubmit'),
+    url(r'^.*$', 'views.login', name='catch-all'),
 ]
